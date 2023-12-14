4970201adeddd9767c15946236aa778396a274aa
Ashish Jha <51054573+ashishjv1@users.noreply.github.com>
2023-12-14 20:32:46 +0545

    Update replacement_utils.py
    Added batchnorm_calibration

--- utils/replacement_utils.py
+++ utils/replacement_utils.py
@@ -3,7 +3,47 @@ import torch
 from torchvision import datasets, transforms, models
 from torch import nn
 
+def batchnorm_callibration(model, train_loader, n_batches = 200000//512,
+                           layer_name = None, device="cuda:0"):
+    '''
+    Update batchnorm statistics for layers after layer_name
+    
+    Parameters:
 
+    model                   -   Pytorch model
+    train_loader            -   Training dataset dataloader, Pytorch Dataloader
+    n_callibration_batches  -   Number of batchnorm callibration iterations, int
+    layer_name              -   Name of layer after which to update BN statistics, string or None
+                                (if None updates statistics for all BN layers)
+    device                  -   Device to store the model, string
+    '''
+    
+    # switch batchnorms into the mode, in which its statistics are updated
+    model.to(device).train() 
+
+    if layer_name is not None:
+        #freeze batchnorms before replaced layer
+        for lname, l in model.named_modules():
+
+            if lname == layer_name:
+                break
+            else:
+                if (isinstance(l, nn.BatchNorm2d)):
+                    l.eval()
+
+    with torch.no_grad():            
+
+        for i, (data, _) in enumerate(train_loader):
+            _ = model(data.to(device))
+
+            if i > n_batches:
+                break
+            
+        del data
+        torch.cuda.empty_cache()
+        
+    model.eval()
+    return model
 
 def get_layer_by_name(model, mname):
     '''
